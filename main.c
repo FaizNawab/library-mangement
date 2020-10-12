ac1274643c7c2981163f5bdf8427fb957a938f16
Faiz Nawab <nawabfaiz313@gmail.com>
2020-10-12 16:14:30 +0530

    structure and testing function

--- main.c
+++ main.c
@@ -2,8 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include "library.h"
+#include "date.h"
 
 
+void date_tester()
+{
+    date_t d1 = {1,1,2000},d2={31,12,2000};
+    date_t d = {1,1,2000};
+    date_t r = date_add(d,366);
+    date_print(&r);
+
+    int diff = date_cmp(d1,d2);
+    printf("Date Difference : %d\n",diff);
+
+}
+
+void test()
+{
+    user_t u;
+    book_t b;
+    user_accept(&u);
+    user_display(&u);
+    book_accept(&b);
+    book_display(&b);
+
+}
 int main()
 {
     //common functionality area
@@ -18,10 +41,11 @@ int choice;
                     exit(0);
             
             case 1:
-                    signin();
+                    signup();
                     break;
 
-            case 2:
+            case 2: 
+                    signin();
                     break;
             
         }
